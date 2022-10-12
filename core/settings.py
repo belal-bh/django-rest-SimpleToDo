d20a549d36ec51713c4515db2af8c6903795efdf
Md. Belal Hossain <belal.cseai@gmail.com>
2022-10-12 12:21:14 +0600

    adds STATICFILES_DIRS in settings

--- core/settings.py
+++ core/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR.joinpath("staticfiles"),
+]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
